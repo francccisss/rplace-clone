13e3178c73d23674f1462be73f5f10c7ac513286
francis <franzdiaz460@gmail.com>
2023-07-26 17:08:45 +0400

    testing out websocket

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ var app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-var indexRouter = require("./routes/index");
 
 // view engine setup
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "app"));
 app.set("view engine", "pug");
 
-app.use("/", indexRouter);
+app.use("/", (req, res, next) => {
+  res.render("index", { title: "Lol" });
+});
 
 io.on("connection", (socket) => {
   console.log(`user ${socket.id} connected`);
   socket.on("message", (msg) => {
-    io.emit("message", `user ${socket.id}: sent a message: ${msg}`);
+    io.emit("message", { sender: socket.id, msg });
+  });
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
   });
 });
 
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,14 +3,26 @@ const socket = io();
 console.log("init");
 
 const btn = document.querySelector(".btn");
+const input = document.querySelector("#input");
+const messages = document.querySelector("#chat-messages");
 let add = 0;
 
-socket.on("message", (text) => {
-  console.log(text);
+socket.on("message", ({ sender, msg }) => {
+  console.log(msg);
+  const message = document.createElement("li");
+  message.textContent = `${sender} said: ${msg}`;
+  messages.appendChild(message);
+});
+
+socket.on("connect", () => {
+  console.log("client is connected ");
+});
+socket.on("disconnect", (reason) => {
+  socket.emit("board state", "new board state");
+  console.log("client is disconnected");
 });
 
 btn.addEventListener("click", () => {
-  socket.emit("message", "yeet");
-  console.log(add);
-  add++;
+  socket.emit("message", input.value);
+  input.value = "";
 });
